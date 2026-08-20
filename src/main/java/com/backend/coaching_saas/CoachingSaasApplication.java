e2bd99704d9c6101101628e3ef72faf574578211
harpreetsi02 <ninjasingh27619@gmail.com>
2026-08-21 01:35:52 +0530

    first commit

---
+++ src/main/java/com/backend/coaching_saas/CoachingSaasApplication.java
@@ -0,0 +1,13 @@
+package com.backend.coaching_saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachingSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachingSaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/coaching_saas/CoachingSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.coaching_saas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachingSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
